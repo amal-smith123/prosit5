c2e3552ce50733d68aa882fd1be2db2dce709514
Amal Ghazouani <160279971+amal-smith123@users.noreply.github.com>
2025-11-07 19:44:04 +0100

    Add files via upload

---
+++ Java-prosit7/Java-prosit7/src/tn/esprit/gestionzoo/exceptions/ZooFullException.java
@@ -0,0 +1,7 @@
+package tn.esprit.gestionzoo.exceptions;
+
+public class ZooFullException extends RuntimeException {
+    public ZooFullException(String message) {
+        super(message);
+    }
+}
